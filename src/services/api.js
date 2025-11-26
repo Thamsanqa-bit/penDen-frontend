14fe8e79f8242b8f02cbc77346bbf405caca073f
Thamsanqa Mbodla <thamsanqambodla@gmail.com>
2025-11-26 10:31:46 +0200

    updated api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Use environment variable for base URL
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://api.penden.online/api/'
+  ? 'https://api.penden.online/api'
   : 'http://localhost:8000/api';
 
 const API = axios.create({
