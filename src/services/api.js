af96c9a1660fb7699cbc819c4d3a79abef687351
Thamsanqa Mbodla <thamsanqambodla@gmail.com>
2025-12-19 07:36:21 +0200

    changed backend url

--- src/services/api.js
+++ src/services/api.js
@@ -32,7 +32,7 @@ import axios from "axios";
 
 // Use environment variable for base URL
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://api.penden.online/api/'
+  ? 'https://penden-backend.onrender.com/api/'
   : 'http://127.0.0.1:8000/api/';
 
 const API = axios.create({
