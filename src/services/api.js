4fa57f8e4ec48004103714270661f5ab10ec7106
Thamsanqa Mbodla <thamsanqambodla@gmail.com>
2026-01-06 09:26:06 +0200

    domain changed

--- src/services/api.js
+++ src/services/api.js
@@ -24,7 +24,7 @@ import axios from "axios";
 const API_BASE_URL = process.env.REACT_APP_API_URL || 
   (process.env.NODE_ENV === 'production' 
     ? 'https://api.penden.store/api/'
-    : 'http://127.0.0.1:8000/api/');;
+    : 'http://127.0.0.1:8000/api/');
 
 const API = axios.create({
   baseURL: API_BASE_URL,
